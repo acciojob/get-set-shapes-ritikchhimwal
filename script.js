4359b6b90cf42a43a0f01c06a67a0cdf0db798af
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-11-22 23:00:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Pass side as both width and height to the Rectangle constructor
+  }
+
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
